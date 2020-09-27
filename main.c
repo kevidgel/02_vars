1e4178d46181a9d97edebc85b1f5427ca72b21b8
Steven Lee <stevenpig99@gmail.com>
2020-09-26 23:23:05 -0400

    hi

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+  double number = 100.010101011;
+  unsigned int a = 1111111111;
+  int b = 101134;
+  char man = 'z';
+  unsigned char slee = 122;
+
+  printf("%lf\n", number);
+  printf("%d\n", a);
+  printf("%d\n", b);
+  printf("%c\n", man);
+  printf("%c\n", slee);
+
+  return 0;
+}
